58c37b115b11b7442e50d263b2590921d5e8a4ea
Nicole <Nicole@NICOLE-PC>
2017-12-02 22:45:00 -0200

    Inicializado o projeto no NetBeans.

---
+++ src/exercicios/Array.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios;
+
+/**
+ *
+ * @author Nicole
+ */
+public class Array {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
